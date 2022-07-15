095c913798ab120470a028e1a0b215b3305fbd4d
kibnakamoto <86175813+kibnakamoto@users.noreply.github.com>
2022-07-15 13:45:37 -0400

    Update gen_comp_values.cpp

--- gen_comp_values.cpp
+++ gen_comp_values.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
 #include <stdint.h>
 #include <cmath>
+#include <bitset>
+
+inline uint64_t rr(uint64_t x, unsigned int n) {
+    return (x >> n)|(x << ((sizeof(x)<<3)-n));
+}
+
+inline uint64_t lr(uint64_t x, unsigned int n) {
+    return (x << n)|(x >> ((sizeof(x)<<3)-n));
+}
+
+inline std::string bin(uint64_t x) {
+    return std::bitset<64>(x).to_string();
+}
 
 int main() {
     uint16_t primes[17];
@@ -23,15 +36,32 @@ int main() {
     }
     
     for(int i=0;i<17;i++) std::cout << primes[i] << " ";
+    std::cout << std::endl << std::endl;
     // try: 11th prime number(37),
-    // first prime number (11-7 as index), value=3,
-    // 18th prime number 11+7 as index
-    for (int i=0;i<64;i+=2) {
-        for(int j=0;j<64;j+=3) {
-            for(int j=0;j<64;j+=3) {
-                
-            }
-        }
-    }
+    // try: 1st prime number(3),
+    // try: 16th prime number(59)
+    // try: try 9th prime number(29)
+    
+    uint64_t i=primes[10];
+    uint64_t j=primes[0];
+    uint64_t k=primes[15];
+    uint64_t n=primes[8];
+    
+    // test rotation on single value(test num)
+    uint64_t tn1 = 0x159a300c24f5dc1eULL;
+    uint64_t tn2 = 0x0000000000000000ULL;//0x3030303030303030ULL;
+    uint64_t tn3 = 0x0000000000000000ULL;//0x3030303030303030ULL;
+    uint64_t tn4 = 0x0000000000000000ULL;
+    
+    uint64_t finalA = rr(tn1, i) xor lr(tn2, j) xor (tn3 << k) xor (tn4 << n) & 0xffffffffffffffffULL;
+    uint64_t finalB = rr(tn4, n) xor lr(tn1, i) xor (tn2 << j) xor (tn3 << k) & 0xffffffffffffffffULL;
+    uint64_t finalC = rr(tn3, k) xor lr(tn4, n) xor (tn1 << i) xor (tn2 << j) & 0xffffffffffffffffULL;
+    uint64_t finalD = rr(tn2, j) xor lr(tn3, k) xor (tn4 << n) xor (tn1 << i) & 0xffffffffffffffffULL;
+    uint64_t final = (finalA + finalB + finalC + finalD) & 0xffffffffffffffffULL;
+    
+    std::cout << std::hex << bin(final) << std::endl << std::endl;
+    std::cout << std::hex << bin(tn1) << std::endl;
+    std::cout << std::hex << bin(tn2) << std::endl;
+    std::cout << std::hex << bin(tn3) << std::endl;
     return 0;
 }
