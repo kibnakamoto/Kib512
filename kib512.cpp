83b46d753eb4f4fa0feea56fc596880aa2b60757
kibnakamoto <86175813+kibnakamoto@users.noreply.github.com>
2022-07-15 14:07:17 -0400

    Create kib512.cpp

--- kib512.cpp
+++ kib512.cpp
@@ -1,34 +1,22 @@
 #include <iostream>
 #include <stdint.h>
-#include <string>
-#include <sstream>
-#include <iomanip>
 #include <cmath>
-#include <bit>
-#include <assert.h>
+#include <bitset>
 
-// IMPORTANT:
-// both the 2-d and 3-d matrix are correct. only thing is that data is big endian.
+inline uint64_t rr(uint64_t x, unsigned int n) {
+    return (x >> n)|(x << ((sizeof(x)<<3)-n));
+}
 
-#if !defined(UINT8_MAX)
-    using uint8_t = unsigned char
-#elif !defined(UINT32_MAX)
-    using uint32_t = unsigned int
-#elif !defined(UINT64_MAX)
-    using uint64_t = unsigned long long
-#endif
+inline uint64_t lr(uint64_t x, unsigned int n) {
+    return (x << n)|(x >> ((sizeof(x)<<3)-n));
+}
 
-// pre-processing of kib512
-class Kib512 {
-    private:
-    uint64_t m_ch;
-    
-    public:
-    static constexpr uint64_t const const_m[8][8] = {
-        {0x159a300c24f5dc1eULL, 0x178f1324ac498bfcULL, 0x10e55f6926814653ULL,
-         0x1963cf80d6276cccULL,0x10980aa9695d807aULL, 0x1703f56bbe1f7f5eULL,
-         0x16eb052c4abc81feULL, 0x1f190bb16583b88fULL}, {0x1d6d15eb483d1a05ULL,
-         0x1e2c7d2c722534b7ULL, 0x14dcbefdb2afe52dULL, 0xf5fbc705a80df745ULL,
+inline std::string bin(uint64_t x) {
+    return std::bitset<64>(x).to_string();
+}
+
+/*
+     0x1e2c7d2c722534b7ULL, 0x14dcbefdb2afe52dULL, 0xf5fbc705a80df745ULL,
          0xe1803d026daf382fULL, 0x4817a3bcb90fa1deULL, 0x3b8d9167a61165bdULL,
          0x400b71315483aecfULL}, {0x7e253caa1049536cULL, 0x7143a810657de4e0ULL,
          0xba3d16917c91aae3ULL, 0x249b4da86d970a00ULL, 0x290ffdedae0a1c66ULL,
@@ -38,156 +26,61 @@ class Kib512 {
          0xcccd035627a53b51ULL, 0x13d653d8c3ae0192ULL}, {0x5d1e80c0123206a1ULL,
          0x91b1b3f312ac1be8ULL, 0x19bc3eed09a866a2ULL, 0x29ed711bc0303fb1ULL,
          0x35fa6009ed810bd6ULL, 0x69a90fb1b95d63aeULL, 0x38bedd43ad759c34ULL,
-         0xa787e2edc8133e84ULL}, {0x552c119614a1f4e5ULL, 0x68d39af3ec9b831fULL,
-         0x3996ac49f898e965ULL, 0x8d79f80825eb64f1ULL, 0x5babe19fdfe4ec70ULL, 
-         0x7aa6a18653c9a0ccULL, 0x510e3b69d44d759aULL, 0x3486650c8e18c529ULL},
-        {0x2b2b4051acc6e9a5ULL, 0x49a9817e3d56ff74ULL, 0xc6fd82e30671aeb5ULL,
-         0x20be895e589409fdULL, 0xd8adec17910a1d4fULL, 0x18882870301f5a0bULL,
-         0x225cb56188423cf3ULL, 0x4b61cc25258ea5c8ULL}, {0x63f68eef219e6c01ULL,
-         0x7c29a354966d3f55ULL, 0x1cf4c5f3b98ce4deULL, 0x1476b731259de5f8ULL, 
-         0x73642b2ec6c2a44bULL, 0x407adff58f6e054cULL, 0x811f7cbc6a7e08c7ULL,
-         0x277c0323eb636b90ULL}
-    };
-    
-    mutable uint64_t hash[8] {
-        0x5287173768046659ULL, 0x1388c1a81885db29ULL, 0xc582055c7b0f1a24ULL,
-        0x9be22d058c2ae082ULL, 0xa36b2344c3b2e0d0ULL, 0x8b74c2c08074d4b1ULL,
-        0x2a1c87eb1011bd80ULL, 0x64edcba54a4d0a5aULL
-    };
-    
-    uint8_t** kib512_prep(std::string input)
-    {
-        uint8_t** matrix = nullptr;
-        std::stringstream ss;
-        
-        // matrix column height
-        uint64_t len = input.length();
-        uint32_t pad = (((512-((len*8)+1)-128) % 512)-7)/8;
-        m_ch = (pad + len + 17)/8;
-        
-        // declare 8 by multiple of 8 matrix
-        matrix = new uint8_t*[8];
-        for(int c=0;c<8;c++) matrix[c] = new uint8_t[m_ch];
-        
-        // add delimeter to denote end of data
-        input+=0x90;
-        
-        // padding
-        for(int c=0;c<pad;c++) input+='0';
-        
-        // add length of input in hex to end of input input
-        ss << std::setfill('0') << std::setw(16) << std::hex << len;
-        input+=ss.str();
-        
-        // 1-d array to 2-d matrix
-        for(int r=0;r<8;r++) {
-            for(uint64_t c=0;c<m_ch;c++) {
-                matrix[r][c] = (uint8_t)input[r*m_ch+c]; // input ordered as row
-            }
-        }
-        
-        // for(int r=0;r<8;r++) {
-        //     for(uint64_t c=0;c<m_ch;c++) {
-        //         std::cout << std::hex << matrix[r][c]+0;
-        //     }
-        // }std::cout << std::endl;
-        
-        return matrix;
-    }
-    
-    uint64_t ***prec_kib512(uint8_t **matrix)
-    {
-        // for compression have 
-        // initialize manipulation matrix with input matrix
-        uint64_t ***manip_m = nullptr;
-        manip_m = new uint64_t **[m_ch/8];
-        uint64_t loop_count = 0;
-        
-        // initialize 3-d matrix to zero to indexes that won't be initialized to
-        // values of 2-d matrix
-        for(int i=0;i<m_ch/8;i++) {
-            manip_m[i] = new uint64_t *[8];
-            manip_m[i][0] = new uint64_t[8];
-            for(int j=1;j<8;j++) { // to avoid unnecesary padding, j=1
-                manip_m[i][j] = new uint64_t[8];
-                for(int k=0;k<8;k++) manip_m[i][j][k] = 0x0000000000000000ULL;
-            }
-        }
-        
-        uint64_t mmi=0; // manipulation matrix i
-        int mmj=0;  // manipulation matrix j
-        for(int j=0;j<8;j++) {
-            for(uint64_t i=0;i<m_ch/8;i++) {
-                uint64_t temp=0;
-                // add matrix values while avoiding repetition of values
-                for(int x=0;x<8;x++) {
-                    temp or_eq (uint64_t)matrix[j][x+i*8] << 56-x*8;
-                }
-                
-                // data in matrix has to be big-endian
-                if constexpr(std::endian::native == std::endian::little) {
-                    manip_m[mmi][0][mmj] = __builtin_bswap64(temp &
-                                                             0xffffffffffffffffULL);
-                } else {
-                    manip_m[mmi][0][mmj] = temp & 0xffffffffffffffffULL;
-                }
-                std::cout << mmi << "\n";
-                mmj = (mmj+1)%8;
-            }
-            if(mmj%8==0) mmi++;
-        }
-        
-        // pre-compression. Get rid of extra padding
-        for(uint64_t i=0;i<m_ch/8;i++) {
-            for(int j=1;j<8;j++) {
-                for(int k=0;k<8;k++) {
-                    
-                }
-            }
-        }
-        return manip_m;
-    }
-    
-    void hash_kib512(uint64_t*** manip_m); // compression function
-    
-    template<typename T>
-    T hashstr(std::string input) {
-        // check if type of T is valid, then return hash
-        assert((typeid(T) != typeid(std::string) || typeid(T) !=
-                typeid(uint64_t*)) && "Kib512 hashstr:\t wrong type detected");
-        
-        // calculate hash
-        uint8_t **m = kib512_prep(input);
-        uint64_t ***manipm = prec_kib512(m);
-        hash_kib512(manipm);
-        
-        // return hash as requested data type
-        if(typeid(T) == typeid(std::string)) {
-            std::stringstream ss;
-            for(int i=0;i<8;i++) {
-                ss << std::setfill('0') << std::setw(16) << std::hex << hash[i];
-            }
-            return ss.str();
-        }
-        else {
-            return hash;
-        }
-    }
-};
+         0xa787e2edc8133e84ULL}
+*/
 
-int main()
-{
-    Kib512 kib512 = Kib512();
-    std::string in = "abcdefghqwertyuioplkjhgfdsazxcvbnm1234567890!@#$\%^&*()\\/";
-    uint8_t **m = kib512.kib512_prep(in);
-    uint64_t ***manipm = kib512.prec_kib512(m);
-    
-    
-    std::cout << "\n\n" << std::hex << in.length();
-    for(int r=0;r<8;r++) {
-        for(int c=0;c<8;c++) {
-            // std::cout << m[r][c];
+int main() {
+    uint16_t primes[17];
+    uint16_t num = 3;
+    int p_index = 0;
+    bool isprime;
+    while(p_index != sizeof(primes)/sizeof(primes[0])) {
+        isprime = true;
+        for(int i=2;i<=num/2;i++) {
+            if(num%i == 0) {
+                isprime = false;
+                break;
+            }
         }
+        if(isprime) {
+            primes[p_index] = num;
+            p_index++;
+        }
+        num++;
     }
+    
+    for(int i=0;i<17;i++) std::cout << primes[i] << " ";
+    std::cout << std::endl << std::endl;
+    // try: 11th prime number(37),
+    // try: 1st prime number(3),
+    // try: 16th prime number(59)
+    // try: try 9th prime number(29)
+    
+    uint64_t i=primes[10];
+    uint64_t j=primes[0];
+    uint64_t k=primes[15];
+    uint64_t n=1;//primes[8];
+    
+    // test rotation on single value(test num)
+    uint64_t tn1 = 0x159a300c24f5dc1eULL;
+    uint64_t tn2 = 0x3030303030303030ULL;
+    uint64_t tn3 = 0x3030303030303030ULL;
+    uint64_t tn4 = 0x0000000000000000ULL;
+    
+    uint64_t finalA = rr(tn1, i) xor lr(tn2, j) xor (tn3 << k) xor (tn4 << n) & 0xffffffffffffffffULL;
+    uint64_t finalB = rr(tn4, n) xor lr(tn1, i) xor (tn2 << j) xor (tn3 << k) & 0xffffffffffffffffULL;
+    uint64_t finalC = rr(tn3, k) xor lr(tn4, n) xor (tn1 << i) xor (tn2 >> j) & 0xffffffffffffffffULL;
+    uint64_t finalD = rr(tn2, j) xor lr(tn3, k) xor (tn4 << n) xor (tn1 >> i) & 0xffffffffffffffffULL;
+    uint64_t final = (finalA + finalB + finalC + finalD) & 0xffffffffffffffffULL;
+    
+    std::cout << std::hex << final << std::endl;
+    std::cout << std::hex << bin(finalA) << std::endl;
+    std::cout << std::hex << bin(finalB) << std::endl;
+    std::cout << std::hex << bin(finalC) << std::endl;
+    std::cout << std::hex << bin(finalD) << std::endl << std::endl;
+
+    std::cout << std::hex << bin(tn1) << std::endl;
+    std::cout << std::hex << bin(tn2) << std::endl;
+    std::cout << std::hex << bin(tn3) << std::endl;
     return 0;
 }
